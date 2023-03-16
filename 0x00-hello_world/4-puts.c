efeed3923fd1a4f3ee0c35763d6beba7a457ec28
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-16 14:04:33 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /*
+ *
  * main - A  program that print a line with puts function and not printf
- */
+ *
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
