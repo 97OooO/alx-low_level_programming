754c760989e7665f6e5e761765f53a0b200caf24
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-16 12:52:29 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /*
- * program that print a line with puts function and not printf 
+ * main - A  program that print a line with puts function and not printf 
  */
 int main(void)
 {
