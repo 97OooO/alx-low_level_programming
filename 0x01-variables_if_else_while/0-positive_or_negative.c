49a6539667574c71bbabb6eb449baf184e5950eb
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-20 13:54:22 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,19 +8,21 @@
  */
 int main(void)
 {
-        srand(time(NULL)); 
+        int n;
 
-        int n = rand() % 201 - 100;
-
-        printf("%d\n", n);
-
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
         if (n > 0)
-                printf("%d is positive\n",n);
+        {
+                printf("%d is positive\n", n);
+        }
         else if (n == 0)
-                printf("%d is zero\n",n);
+        {
+                printf("%d is zero\n", n);
+        }
         else
-                printf("%d is negative\n",n);
-
+        {
+                printf("%d is negative\n", n);
+        }
         return (0);
 }
-
