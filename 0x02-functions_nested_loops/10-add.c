ab12e3dfd23a87691083bfd995c2d4373e4be683
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-21 16:51:30 -0700

    Sum of two integers O and L

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * add - adds 2 integers and return an integer as a result
+ * @a: integer to be added to b
+ * @b: integer to be added to a
+ * Return: sum of a and b
+ */
+
+int add ( int O, int L)
+{
+	int sum;
+
+	c = O + L;
+
+	return (sum);
+}
