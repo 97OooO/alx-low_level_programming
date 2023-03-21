66426756d7a4c95da18acb273f81d8b7a29aeb1c
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-21 15:40:20 -0700

    Takes an integer and returns its absolute value as an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * _abs - takes an integer as input and
+ * return it's absolute value as an integer
+ *
+ * @nmb: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int nmb)
+{
+	if (nmb < 0)
+	{
+		return -nmb;
+	}
+	else
+	{
+		return nmb;
+	}
+}
