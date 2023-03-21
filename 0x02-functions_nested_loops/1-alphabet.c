f17a69695a232d31fe09e036b62f3157893c2e05
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-21 13:05:49 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
+nclude "main.h"
 
+/**
+ *print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c[] = "abcdefghijklmnoprstuvwxyz\n";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchr(c[i]);
+		_putchar('\n');
+		i++;
+	}
+
+	return (0);
+}
