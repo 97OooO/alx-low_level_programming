232ec21a5c03f9d8c5022372955adf247eac54aa
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-21 13:48:11 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - print 10 time the alphabet, in lowercase and followed by new line.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i =0;
+
+	letter ='a';
+
+	while (i<10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
