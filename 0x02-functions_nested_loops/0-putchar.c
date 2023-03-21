cd6957a26e7cb5bbefa02ff5717f7f3691fee873
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-21 11:35:53 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
-#include "main.h"
+#include"main.h"
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
 {
 	char c[] = "_putchar\n";
 	int i = 0;
