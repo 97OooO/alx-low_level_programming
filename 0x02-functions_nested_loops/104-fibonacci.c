3f9e840347c3d062358b6c0d378ca107047ad311
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-22 15:34:27 -0700

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return: (Success)
+ */
+
+int main()
+{
+	int i, fib1 = 1, fib2 = 2, fib_next;
+
+	printf("%d, %d, ", fib1, fib2);
+
+	for (i = 3; i <= 98; i++)
+	{
+		fib_next = fib1 + fib2;
+		printf("%d", fib_next);
+
+		if (i != 98)
+		{
+			printf(", ");
+		}
+
+		fib1 = fib2;
+		fib2 = fib_next;
+	}
+	printf("\n");
+	return (0);
+}
