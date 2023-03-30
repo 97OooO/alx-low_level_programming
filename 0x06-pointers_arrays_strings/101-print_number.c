57f1053cca6c8e79df7c055bbedcf82ae0346760
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-03-30 15:26:50 -0700

    print numbers chars

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,16 +12,15 @@ void print_number(int n)
 
 	m = n;
 
-	if (m / 10 != 0)
-        {
-                print_number(m / 10);
-        }
-
 	if (n < 0)
 	{
 		_putchar('-');
 		m = -n;
 	}
 
+	if (m / 10 != 0)
+        {
+                print_number(m / 10);
+        }
 	_putchar((m % 10) + '0');
 }
