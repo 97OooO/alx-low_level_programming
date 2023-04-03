da81eef698533e3da714ede99b861ae099387e49
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-04-03 15:38:11 -0700

    0x07

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,21 +10,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j, len;
+	unsigned int i, j, len;
 
-    len = 0;
-    for (i = 0; s[i]; i++)
-    {
-        for (j = 0; accept[j]; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                len++;
-                break;
-            }
-        }
-        if (!accept[j])
-            break;
-    }
-    return (len);
+	len = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+		if (!accept[j])
+			break;
+	}
+	return (len);
 }
