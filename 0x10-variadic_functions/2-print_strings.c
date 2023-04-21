54542e87fb390a01ba503261895a5129ff09df97
LAKRIM <ossama.lakrim@uit.ac.ma>
2023-04-21 14:58:23 -0700

    Prints Strings followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,45 +12,46 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        va_list args;
-        unsigned int i;
-        const char *str;
+	va_list args;
+	unsigned int i;
+	const char *str;
 
-        va_start(args, n);
+	va_start(args, n);
 
-        str = va_arg(args, const char *);
-        if (str != NULL)
-        {
-                printf("%s", str);
-                for (i = 1; i < n; i++)
-                {
-                        str = va_arg(args, const char *);
+	str = va_arg(args, const char *);
+	if (str != NULL)
+	{
+		printf("%s", str);
+		for (i = 1; i < n; i++)
+		{
+			str = va_arg(args, const char *);
 
-                        if (separator != NULL)
-                                printf("%s", separator);
-                        if (str == NULL)
-                                printf("(nil)");
-                        else
-                                printf("%s", str);
-                }
-        }
-        else
-        {
-                printf("(nil)");
+			if (separator != NULL)
+				printf("%s", separator);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+		}
+	}
+	else
+	{
+		printf("(nil)");
 
-                for (i = 1; i < n; i++)
-                {
-                        str = va_arg(args, const char *);
-                        if (separator != NULL)
-                                printf("%s", separator);
-                        if (str == NULL)
-                                printf("(nil)");
-                        else
-                                printf("%s", str);
-                }
-        }
+		for (i = 1; i < n; i++)
+		{
+			str = va_arg(args, const char *);
+			if (separator != NULL)
+				printf("%s", separator);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+		}
 
-        printf("\n");
+	}
 
-        va_end(args);
+	printf("\n");
+
+	va_end(args);
 }
