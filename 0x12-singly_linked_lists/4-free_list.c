8a791d12dd5e2794a402e55eea3bfdd42f65b549
LAKRIM <lakrimoussama@gmail.com>
2023-04-28 16:41:38 -0700

    Function frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @h: list_t list to be freed
+ */
+void free_list(list_t *h)
+{
+	list_t *tmp;
+
+	while (h)
+	{
+		tmp = h->next;
+		free(h->str);
+		free(h);
+		h = tmp;
+	}
+}
