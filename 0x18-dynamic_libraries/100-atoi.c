19feca67a85615d8befe257fa4eebf6e41e4b607
LAKRIM <lakrimoussama@gmail.com>
2023-08-21 23:59:06 +0100

    Task0

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1, num = 0, i = 0;
+
+    /* In this stage: Skip whitespace characters*/
+	while (*(s + i) == ' ' || *(s + i) == '\n' || *(s + i) == '\t')
+		i++;
+
+    /* In this stage: Handle negative sign if present*/
+	if (*(s + i) == '-')
+	{
+		sign = -1;
+		i++;
+	}
+    /* In this stage: Handle positive sign if present*/
+	else if (*(s + i) == '+')
+	{
+		i++;
+	}
+
+    /* In this stage: convert string to integer*/
+	while (*(s + i) >= '0' && *(s + i) <= '9')
+	{
+		num = num * 10 + (*(s + i) - '0');
+		i++;
+	}
+
+	return (sign * num);
+}
---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: 0 if the strings are equal, 
+ * negative value if s1 < s2, 
+ * positive value if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+
+	else if (s1[i] == '\0')
+	{
+		return (-s2[i]);
+	}
+
+	else
+	{
+		return (s1[i]);
+	}
+}
